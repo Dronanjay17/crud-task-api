9afa4cde72ac42889a02fb4c8c590cf64947dce4
Dronanjay17 <amohabe61@gmail.com>
2022-02-27 19:06:13 +0530

    added express and mongoose configuration

---
+++ database/mongoose.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://127.0.0.1:27017/taskmanager')
+
+.then(()=>{
+    console.log('Database Successfully Connected');
+})
+.catch((error)=>{
+    console.log(error);
+});
+
+module.exports = mongoose;
+
